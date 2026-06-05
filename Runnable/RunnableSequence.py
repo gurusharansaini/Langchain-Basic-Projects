d0826b24aac82f0176f2f77c5360883afdca77a7
Gurusharansaini <Gurusharansaini4@gmail.com>
2026-06-05 16:02:26 +0530

    worked on RunnableSequence.py

---
+++ Runnable/RunnableSequence.py
@@ -0,0 +1,25 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import RunnableSequence
+
+load_dotenv()
+
+prompt = PromptTemplate(
+    template="write a joke about {topic}",
+    input_variables = ['topic']
+)
+prompt2 = PromptTemplate(
+    template="explain the following joke {text}",
+    input_variables=['text']
+)
+
+model = ChatOpenAI()
+parser = StrOutputParser()
+
+chain = RunnableSequence(prompt,model,parser,prompt2,model,parser)
+
+result = chain.invoke({'topic':'AI'})
+
+print(result)
